25f53278f4d7112bf6eb8952ca80a1bc40a25c92
LahcenHaouch <lahcen.haouch@protonmail.com>
2021-06-29 13:52:30 +0200

    Solve valueAt

--- src/history.ts
+++ src/history.ts
@@ -9,8 +9,27 @@ export interface History<T> {
 }
 
 export const valueAt = <T>(history: History<T>, date: string): T => {
-  // @ts-ignore
-  return // TODO
+  const { steps, defaultValue } = history
+
+  if (!steps.length || date < steps[0].date) {
+    return defaultValue
+  }
+
+  for (let i = 0; i < steps.length; i++) {
+    const { date: currentStepDate, value } = steps[i]
+
+    if (currentStepDate === date || i === steps.length - 1) {
+      return value
+    }
+
+    const { date: nextStepDate } = steps[i + 1]
+
+    if (currentStepDate < date && nextStepDate > date) {
+      return value
+    }
+  }
+
+  return defaultValue
 }
 
 export const add = (histories: History<number>[]): History<number> => {
