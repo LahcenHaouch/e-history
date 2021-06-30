9b4db6581524b4e028223b6d36c0b1bc4e655987
LahcenHaouch <lahcen.haouch@protonmail.com>
2021-06-30 17:47:52 +0200

    Solve add

--- src/history.ts
+++ src/history.ts
@@ -32,28 +32,105 @@ export const valueAt = <T>(history: History<T>, date: string): T => {
   return defaultValue
 }
 
-const mergetHistories = (
+const mergeHistories = (
   firstHistory: History<number>,
-  secondHistory: History<number>
-): History<Number> | undefined => {
-  //
+  secondHistory: History<number>,
+  index: number,
+  histories: Array<History<number>>
+): History<number> => {
+  let biggerHistory: History<number>, smallerHistory: History<number>
 
-  const result = {
-    defaultValue: firstHistory.defaultValue + secondHistory.defaultValue,
+  const previousHistoryDefaultValue = histories[index]?.defaultValue
+
+  const firstIsBigger = previousHistoryDefaultValue
+    ? previousHistoryDefaultValue > secondHistory.defaultValue
+    : firstHistory.defaultValue > secondHistory.defaultValue
+
+  if (firstIsBigger) {
+    biggerHistory = firstHistory
+    smallerHistory = secondHistory
+  } else {
+    biggerHistory = secondHistory
+    smallerHistory = firstHistory
   }
 
-  return undefined
+  const result: History<number> = {
+    defaultValue: biggerHistory.defaultValue + smallerHistory.defaultValue,
+    steps: smallerHistory.steps.map(({ date, value }) => {
+      const biggerHistorySameStepValue = biggerHistory.steps.find(
+        (step) => step.date === date
+      )?.value
+
+      const filteredSmallerStepValues = biggerHistory.steps
+        .filter((step) => step.date < date)
+        .map((step) => step.value)
+
+      const biggerHistorySmallerStepValue = filteredSmallerStepValues.length
+        ? Math.max(...filteredSmallerStepValues)
+        : biggerHistory.defaultValue
+
+      return {
+        value:
+          value +
+          (biggerHistorySameStepValue
+            ? biggerHistorySameStepValue
+            : biggerHistorySmallerStepValue
+            ? biggerHistorySmallerStepValue
+            : biggerHistory.defaultValue),
+        date,
+      }
+    }),
+  }
+
+  result.steps.push(
+    ...biggerHistory.steps
+      .filter(
+        (step) =>
+          !smallerHistory.steps.some(
+            (resultStep) => resultStep.date === step.date
+          )
+      )
+      .map(({ date, value }) => {
+        const filteredSmallerSteps = smallerHistory.steps
+          .filter((step) => step.date < date)
+          .map((step) => step.value)
+
+        return {
+          value:
+            value +
+            (filteredSmallerSteps.length
+              ? Math.max(...filteredSmallerSteps)
+              : 0),
+          date,
+        }
+      })
+  )
+
+  result.steps.sort((stepOne, stepTwo) =>
+    stepOne.date > stepTwo.date ? 1 : -1
+  )
+
+  return result
 }
 
 export const add = (histories: History<number>[]): History<number> => {
-  return histories.reduce(
-    (previousValue, currentValue) => ({
-      defaultValue: previousValue.defaultValue + currentValue.defaultValue,
-      steps: [...previousValue.steps, ...currentValue.steps],
-    }),
+  const addHistories = [...histories].sort(
+    (firstHistory, secondHistory) =>
+      firstHistory.defaultValue - secondHistory.defaultValue
+  )
+
+  const result = addHistories.reduce(
+    (previousValue, currentValue, index) =>
+      mergeHistories(previousValue, currentValue, index - 1, addHistories),
     {
       defaultValue: 0,
       steps: [],
     }
   )
+
+  result.steps.sort((stepOne, stepTwo) =>
+    stepOne.date > stepTwo.date ? 1 : -1
+  )
+
+  return result
 }
